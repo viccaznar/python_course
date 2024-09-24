ac0e9bc11446fd7edcbcfb72609502d00edf80b2
Vicc Aznar <viccaznar@yahoo.com>
2024-09-24 20:38:05 -0300

    Python Comments and Docstrings

---
+++ aula1.py
@@ -0,0 +1,13 @@
+"""
+Docstrings são iniciados por aspas tríplas (""" """) e aspas tríplas
+simples(''' ''').
+Apesar da funcionalidade como bloco de cometários onde se podem
+pular linhas:
+
+Docstrings NÃO SÃO COMENTÁRIOS que são ignorados e são executados sempre
+que o compilador for requisitado para iniciar o programa.
+"""
+
+# (Cerquilha ou Hashtag) Permite escrever um comentário (Em uma única linha).
+print(123) # Na frente do código
+# Abaixo do código
