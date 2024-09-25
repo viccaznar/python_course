9178703551c9eac5e9385b611f214a881c3f5824
Vicc Aznar <viccaznar@yahoo.com>
2024-09-24 22:21:22 -0300

    Python Data Types - part 1

---
+++ aula3.py
@@ -0,0 +1,27 @@
+'''
+Python = Linguagem de programação.
+Tipos de tipagem: Dinâmica / Forte.
+str -> String = cadeia de caracteres (Texto).
+
+    - Strings são textos que estão contidos entre aspas " ".
+'''
+
+print(1234)
+
+# Aspas simples
+print('Luis Otávio')
+
+# Aspas duplas
+print("Luis Otávio")
+
+# Aspas duplas dentro de Aspas simples
+print(1, 'Luis "Otávio"')
+
+# Aspas simples dentro de Aspas duplas
+print(2, "Luis 'Otávio'")
+
+# Caracter de Escape
+print("Luis \"Otávio\"")
+
+# r
+print(r"Luis \"Otávio\"")
