a7d3c6fab783b5160e2d3b881a942385f3126f9a
Vicc Aznar <viccaznar@yahoo.com>
2024-09-25 19:58:41 -0300

    BMI calculation

---
+++ aula12.py
@@ -0,0 +1,8 @@
+nome = 'Luiz Otávio'
+altura = float('1.80')
+peso = 95
+imc = ... # IMC = peso / (altura * altura)
+
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e seu IMC é', imc)
