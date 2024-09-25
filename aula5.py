e2dfb4294ef1179b72c61a14627f9967feb213e3
Vicc Aznar <viccaznar@yahoo.com>
2024-09-24 23:15:24 -0300

    Python Data Types - part 3

---
+++ aula5.py
@@ -0,0 +1,22 @@
+'''
+Tipo de Dado 'bool' (boolean)
+
+Ao questionar algo em um programa, só existem duas respostas possiveis:
+    - Sim (True)
+    - Não (False)
+'''
+
+'''
+Existem vários operadores para "questionar". Dentre eles:
+    
+    - ==, que é um operador lógico que questiona se um valor é igual a outro.
+'''
+
+print(10 == 10) # Sim -> Verdadeiro (True)
+print(10 == 11) # Não -> Falso (False)
+
+print(type(10 == 10)) # Sim -> Verdadeiro (True)
+print(type(10 == 11)) # Não -> Falso (False)
+
+print(type(True)) # Sim -> Verdadeiro (True)
+print(type(False)) # Não -> Falso (False)
