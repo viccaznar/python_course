c6c612c553cc5acbc18101baeac7839363b50e6c
Vicc Aznar <viccaznar@yahoo.com>
2024-09-25 19:37:14 -0300

    Mathematical operators and precedence

---
+++ aula10.py
@@ -0,0 +1,8 @@
+concatenacao = 'Luiz' + ' ' + 'Otávio'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+
+tres_vezes_luiz = 3 * 'Luiz'
+print(tres_vezes_luiz)
---
+++ aula11.py
@@ -0,0 +1,11 @@
+'''
+Precedência de Operadores Aritiméticos
+'''
+
+# 1. (n + n) :  parenteses
+# 2. ** : potenciação
+# 3. * / // % : multiplicação, divisão e resto da divisão
+# 4. + - : adição e subtração
+
+conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
