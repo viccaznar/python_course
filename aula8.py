aeff049df6f27edbfcdbc0128c1841f006b0bd43
Vicc Aznar <viccaznar@yahoo.com>
2024-09-25 18:59:47 -0300

    Variables Exercise

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Marcos Vinicius'
+sobrenome = 'Aznar da Silva'
+idade = int('35')
+ano_nascimento = int('1989')
+maior_de_idade = idade >=18
+altura_metros = float('1.89')
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de Nascimento: ', ano_nascimento)
+print('É maior de idade: ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
