87cde862db009ee4313fa82fd278e8e51ab6dcfc
Vicc Aznar <viccaznar@yahoo.com>
2024-09-25 01:13:36 -0300

    Variables - part 1

---
+++ aula7.py
@@ -0,0 +1,25 @@
+'''
+Variáveis são usadas para salvar algo na memória do computador.
+    - PEP8: inicie variáveis com letras minúsculas, números ou underline ( _ ).
+    - ' = ': O sinal de igual é o operador de atribuição.
+            Ele é usado para atribuir um valor a um nome (variável).
+
+Uso: nome_variavel = expressão.
+'''
+
+nome_completo = 'Luis Otávio Miranda'
+print(nome_completo)
+
+soma_dois_mais_dois = 2 + 2
+
+print(nome_completo, soma_dois_mais_dois)
+
+int_um = int('1')
+print(int_um, type(int_um))
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+
+print('Nome: ', nome, 'Idade: ', idade)
+print('É maior de idade? ', maior_de_idade)
