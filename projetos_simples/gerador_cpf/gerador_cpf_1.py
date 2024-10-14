9367410b0568b84e4c218d1451cb0ff091cd099e
Vicc Aznar <viccaznar@yahoo.com>
2024-10-14 15:29:32 -0300

    Gerador de CPF

---
+++ projetos_simples/gerador_cpf/gerador_cpf_1.py
@@ -0,0 +1,45 @@
+
+
+import re
+import sys  
+
+entrada = input('CPF: ')
+
+cpf_enviado_pelo_usuario = re.sub(
+    r'[^0-9]',
+    '',
+    entrada
+    )
+
+entrada_sequencial = entrada == entrada[0] * len(entrada)   
+
+if entrada_sequencial:
+    print('Você mandou dados sequencais')
+    sys.exit()
+
+nove_digitos = cpf_enviado_pelo_usuario[:9]
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digitos in dez_digitos:
+    resultado_digito_2 += int(digitos) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_pelo_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_pelo_usuario} é válido')
+else:
+    print('CPF inválido')
